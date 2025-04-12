d20dd6bf5dac5457a0f24fa4023f886dbf26be67
Mike Barr <m.barr@mac.com>
2025-04-12 18:01:32 +0200

    adds settings scene

--- PrivateDevRoom/Scripts/Global/scene_transition.gd
+++ PrivateDevRoom/Scripts/Global/scene_transition.gd
@@ -7,7 +7,7 @@ extends CanvasLayer
 
 func change_scene(new_scene_path : String):
 	var tween : Tween = get_tree().create_tween()
-	tween.tween_property(fading_texture, "self_modulate" , Color(255, 255 , 255 , 0), fade_in_time)
+	tween.tween_property(fading_texture, "self_modulate" , Color(255, 255 , 255 , 255), fade_in_time)
 	await tween.finished
 	get_tree().change_scene_to_file(new_scene_path)
-	tween.tween_property(fading_texture, "self_modulate" , Color(255, 255 , 255 , 255), fade_out_time)
+	tween.tween_property(fading_texture, "self_modulate" , Color(255, 255 , 255 , 0), fade_out_time)
