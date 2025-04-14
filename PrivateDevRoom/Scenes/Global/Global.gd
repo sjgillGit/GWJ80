abc8b70f8717177d3894bc1495a17e07801a47e7
InVeRnyako <inver.nyako@gmail.com>
2025-04-14 09:34:13 +0300

    pre-merge renames

--- PrivateDevRoom/Scenes/Global/Global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-const SCENES_DICTIONARY : Dictionary[String, String] = {
-	"MainMenu" : "res://PrivateDevRoom/Scenes/main_menu.tscn" ,
-	"MessageBox" : "res://PrivateDevRoom/Scenes/message_box.tscn" ,
-	"Settings" : "res://PrivateDevRoom/Scenes/settings.tscn"
-}
--- PrivateDevRoom/Scripts/Global/Global.gd
+++ PrivateDevRoom/Scripts/Global/Global.gd
@@ -1,1 +1,7 @@
 extends Node
+
+const SCENES_DICTIONARY : Dictionary[String, String] = {
+	"MainMenu" : "res://PrivateDevRoom/Scenes/main_menu.tscn" ,
+	"MessageBox" : "res://PrivateDevRoom/Scenes/message_box.tscn" ,
+	"Settings" : "res://PrivateDevRoom/Scenes/settings.tscn"
+}
