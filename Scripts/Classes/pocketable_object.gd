7df6c4b29869a30b89f6462f9731fe9b6e84eead
sjgillGit <sjgill851@gmail.com>
2025-04-16 08:59:24 -0500

    Enforcing action from review

--- Scripts/Classes/pocketable_object.gd
+++ Scripts/Classes/pocketable_object.gd
@@ -6,17 +6,17 @@ extends InteractableObject
 func grab_in_pocket():
 	# if [MVP request to player UI]:
 	# item data stored in inventory pocket UI
-	disable_existance()
+	disable_existence()
 	# return true # for grab animation, if will be implemented
 
-func disable_existance() -> void:
+func disable_existence() -> void:
 	get_parent().remove_child(self)
 	set_physics_process(false)
 	set_process(false)
 	sleeping = true
 	hide()
 
-func enable_existance(player) -> void:
+func enable_existence(player) -> void:
 	player.get_parent().add_child(self) # MVP should have proper reference to current game scene
 	set_physics_process(true)
 	set_process(true)
