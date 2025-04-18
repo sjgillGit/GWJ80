40c0269ce05be414a6baa8b66cfc44e2816cee77
jozin2626 <jozin2626@gmail.com>
2025-04-18 08:47:32 -0400

    nav_mesh updates

--- PrivateDevRoom/env_nav/npc_pathfinding.gd
+++ PrivateDevRoom/env_nav/npc_pathfinding.gd
@@ -10,23 +10,50 @@ extends CharacterBody3D
 		return accel
 	set (accel_in):
 		accel = accel_in
+var holding_object: bool = false:
+	get:
+		return holding_object
 
+# These values are for determining semi-random positions in the level for pathfinding
+@export var lower_x : float = 0.0
+@export var lower_y : float = 0.0
+@export var lower_z : float = 0.0
+@export var upper_x : float = 0.0
+@export var upper_y : float = 0.0
+@export var upper_z : float = 0.0
+
+# Value for number of locations npc spawned will want to walk towards
+@export var number_of_locations : int = 0
+
+# This is mainly for testing purposes, unless we want npc to only go towards one location
 @export var destination: Vector3 = Vector3.ZERO
-		
+
+# Navigation agent
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
-		
-# For testing, reapply destination with random value - remove before merging with player scripts
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump"):
-		var random_position = Vector3.ZERO
-		random_position.x = randf_range(-10.0, 10.0)
-		random_position.y = randf_range(-10.0, 10.0)
-		nav.set_target_position(random_position)
+
+var paths = []
+
+# The purpose is to get random range of coordinates npc wants to go towards
+func set_random_path_list():
+	var path_list = []
+	for i in range(len(number_of_locations)):
+		var x = randf_range(lower_x, upper_x)
+		var y = randf_range(lower_y, upper_y)
+		var z = randf_range(lower_z, upper_z)
+		path_list.append(Vector3(x, y, z))
+	return path_list
+
+func _ready() -> void:
+	set_random_path_list()
 
 func _physics_process(delta: float) -> void:
 	nav.set_target_position(destination)
-	var dest = nav.target_position      
+	var dest = nav.target_position
 	var local_destination = dest - global_position
-	var direction = local_destination.normalized()
-	velocity = direction * speed
+	var direction = (nav.get_next_path_position() - global_position).normalized()
+	
+	if not holding_object:
+		velocity = (direction * speed * accel) * delta
+	else:
+		velocity = (direction * speed) * delta	
 	move_and_slide()
